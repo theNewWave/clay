ac7873c7cfaef78fa638bbf0c093d305f76efaca
Ryo Sakai <r.sakai@tryhard.me>
2019-05-16 16:10:31 +0900

    revised something

--- src/styles/components/Row/index.js
+++ src/styles/components/Row/index.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components'
 
 export const Row = styled.div`
-  max-width: ${props => (props.maxWidth ? `${props.maxWidth}px` : '960px')};
-  margin-right: auto;
-  margin-left: auto;
-  padding-left: ${props => (props.padding ? `${props.padding}px` : '15px')};
-  padding-right: ${props => (props.padding ? `${props.padding}px` : '15px')};
+  width: 100%;
+  display: flex;
 `
